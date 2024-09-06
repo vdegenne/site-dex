70c570da1463d26e67cc3697890d9e0634d9bccb
vdegenne <vdegenne@gmail.com>
2024-09-06 10:31:13 +0000

    Build: (9432b2e) 2024-09-06

--- assets/index-kHzSurly.js
+++ assets/index-kHzSurly.js
@@ -1,11 +1,11 @@
-import{Q as b,k as m,u as f,A as p,l as k,D as y}from"./index-7kV3smd2.js";import{s as d}from"./item-dialog-Bh728tCR.js";function P({headline:i,content:o,cancelButton:t,confirmButton:a,blockScrimClick:w=!1,blockEscapeKey:g=!1,onDialogReady:c,styles:v}){return new Promise(async(A,h)=>{const s=document.createElement("div");let u=Promise.resolve(),$=Promise.resolve(),n=!1;b(m`
+import{Q as b,l as m,w as f,E as p,q as k,D as y}from"./index-C8WMvdWp.js";import{s as d}from"./item-dialog-DoyA_CP8.js";function P({headline:i,content:o,cancelButton:t,confirmButton:a,blockScrimClick:w=!1,blockEscapeKey:g=!1,onDialogReady:c,styles:v}){return new Promise(async(h,A)=>{const s=document.createElement("div");let u=Promise.resolve(),$=Promise.resolve(),n=!1;b(m`
 				<md-dialog
 					?block-scrim-click="${w}"
 					?block-escape-key="${g}"
 					style="${d(v??{})}"
 					@cancel=${e=>{const l=e.target;l.returnValue===""&&(n?l.hasAttribute("block-escape-key")&&e.preventDefault():l.hasAttribute("block-scrim-click")&&e.preventDefault(),n=!1)}}
 					@keydown=${e=>{e.code==="Escape"&&(n=!0)}}
-					@closed=${async e=>{switch(e.target.returnValue){case"":case"cancel":h(await u);break;case"confirm":default:A(await $)}e.target.remove(),s.remove()}}
+					@closed=${async e=>{switch(e.target.returnValue){case"":case"cancel":A(await u);break;case"confirm":default:h(await $)}e.target.remove(),s.remove()}}
 				>
 				</md-dialog>
 			`,s),document.body.prepend(s);const r=s.querySelector(":scope > md-dialog");await r.updateComplete,b(m`
@@ -38,4 +38,4 @@ import{Q as b,k as m,u as f,A as p,l as k,D as y}from"./index-7kV3smd2.js";impor
 								`})():y}
 							</div>
 					  `:null}
-			`,r),r.$={confirmButton:null,cancelButton:null},r.querySelectorAll("[id]").forEach(e=>{r.$[e.getAttribute("id")]=e}),await r.updateComplete,await(c==null?void 0:c(r)),r.show()})}async function x({headline:i="Are you sure?",content:o="Are you sure to perform this action?",cancelButton:t={},confirmButton:a={}}={}){return await P({headline:i,content(){return o},confirmButton:a,cancelButton:t})}export{x as materialConfirm,P as materialDialog};
+			`,r),r.$={confirmButton:null,cancelButton:null},r.querySelectorAll("[id]").forEach(e=>{r.$[e.getAttribute("id")]=e}),await r.updateComplete,await(c==null?void 0:c(r)),r.show()})}async function E({headline:i="Are you sure?",content:o="Are you sure to perform this action?",cancelButton:t={},confirmButton:a={}}={}){return await P({headline:i,content(){return o},confirmButton:a,cancelButton:t})}export{E as materialConfirm,P as materialDialog};
