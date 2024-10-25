65e8724bb0aa206376b98182fae120cb8caf35f9
vdegenne <vdegenne@gmail.com>
2024-10-25 17:50:17 +0000

    Build: (d5f9ec0) favicon or not

--- assets/index-Cqdsqj5x.js
+++ assets/index-Cqdsqj5x.js
@@ -1,4 +1,4 @@
-import{Q as b,l as m,w as f,E as p,q as k,D as y}from"./index-BzYJ1zyw.js";import{s as d}from"./item-dialog-BgqNsI6L.js";function P({headline:i,content:o,cancelButton:t,confirmButton:a,blockScrimClick:w=!1,blockEscapeKey:g=!1,onDialogReady:c,styles:v}){return new Promise(async(h,A)=>{const s=document.createElement("div");let u=Promise.resolve(),$=Promise.resolve(),n=!1;b(m`
+import{Q as b,l as m,w as f,E as p,q as k,D as y}from"./index-BEGimide.js";import{s as d}from"./item-dialog-DmbEcvi0.js";function P({headline:i,content:o,cancelButton:t,confirmButton:a,blockScrimClick:w=!1,blockEscapeKey:g=!1,onDialogReady:c,styles:v}){return new Promise(async(h,A)=>{const s=document.createElement("div");let u=Promise.resolve(),$=Promise.resolve(),n=!1;b(m`
 				<md-dialog
 					?block-scrim-click="${w}"
 					?block-escape-key="${g}"
